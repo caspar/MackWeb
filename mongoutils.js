03499c5044427fd5d7220ba91946db57ebe300d0
janejoyce <jja8@me.com>
2014-06-14 11:08:05 -0400

    created mongoutils - Jane

---
+++ mongoutils.js
@@ -0,0 +1,5 @@
+/*
+mongoutils plan (for my reference):
+*should be able to addStudent(name, gender, crushes) to db
+*from the list of crushes, need to search the db to see if the names are there and make a new name
+*/
