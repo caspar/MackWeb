5bbfc20d489e6eef42a306abaf00dc4fa3e62b5a
janejoyce <jja8@me.com>
2014-06-14 11:13:24 -0400

    makin database - Jane

--- mongoutils.js
+++ mongoutils.js
@@ -3,3 +3,7 @@ mongoutils plan (for my reference):
 *should be able to addStudent(name, gender, crushes) to db
 *from the list of crushes, need to search the db to see if the names are there and make a new name
 */
+
+conn = new Mongo();
+db = conn.getDB("mackDB");
+
